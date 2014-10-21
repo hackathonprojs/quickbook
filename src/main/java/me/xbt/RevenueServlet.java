a5a24fdee3f4f085ac75f1cb2b7defdc2519d1db
kaye wu <apiswswsw@gmail.com>
2014-10-20 23:48:47 -0700

    refactor constants

--- src/main/java/me/xbt/RevenueServlet.java
+++ src/main/java/me/xbt/RevenueServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import me.xbt.common.AppInfo;
+
 import com.intuit.ipp.core.Context;
 import com.intuit.ipp.core.ServiceType;
 import com.intuit.ipp.data.ColData;
@@ -46,15 +48,16 @@ public class RevenueServlet extends HttpServlet {
 		String netIncome = "";
 		
         // following https://developer.intuit.com/docs/0025_quickbooksapi/0055_devkits/0201_ipp_java_devkit_3.0/0001_synchronous_calls/0001_data_service_apis
- 		String consumerKey = "qyprdjjxjhvmRvpAfaYgHfbVsbz1KH";
- 		String consumerSecret = "mf8ASlocS1DjsZKmThXdWiHkHao81dAvz1TMWZYR";
- 		String accessToken = "qyprdZfe92xmLpx1IiOKvIVIb39oGXMChsO34csqoIGyzgWH";
- 		String accessTokenSecret = "mcSpZrugBMCtBh1hAAFQOhMONsqJTLgk5g1XVPcv";
+ 		String consumerKey = AppInfo.consumerKey;
+ 		String consumerSecret = AppInfo.consumerSecret;
+ 		String accessToken = AppInfo.accessToken;
+ 		String accessTokenSecret = AppInfo.accessTokenSecret;
 
+ 		String appToken = AppInfo.appToken;
+ 		String companyID = AppInfo.companyID; // need to create a company in quickbook connect first.
+ 		
  		OAuthAuthorizer oauth = new OAuthAuthorizer(consumerKey, consumerSecret, accessToken, accessTokenSecret);
  		
- 		String appToken = "1b868c6cb467eb4abeba8bab22a839a6fe0e";
- 		String companyID = "1291602300"; // need to create a company in quickbook connect first.
 
  		Context context = new Context(oauth, appToken, ServiceType.QBO, companyID);
  		
---
+++ src/main/java/me/xbt/common/AppInfo.java
@@ -0,0 +1,13 @@
+package me.xbt.common;
+
+public class AppInfo {
+	// following https://developer.intuit.com/docs/0025_quickbooksapi/0055_devkits/0201_ipp_java_devkit_3.0/0001_synchronous_calls/0001_data_service_apis
+	public static final String consumerKey = "qyprdjjxjhvmRvpAfaYgHfbVsbz1KH";
+	public static final String consumerSecret = "mf8ASlocS1DjsZKmThXdWiHkHao81dAvz1TMWZYR";
+	public static final String accessToken = "qyprdZfe92xmLpx1IiOKvIVIb39oGXMChsO34csqoIGyzgWH";
+	public static final String accessTokenSecret = "mcSpZrugBMCtBh1hAAFQOhMONsqJTLgk5g1XVPcv";
+
+	public static final String appToken = "1b868c6cb467eb4abeba8bab22a839a6fe0e";
+	public static final String companyID = "1291602300"; // need to create a company in quickbook connect first.
+
+}
